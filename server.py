4a2b701c91216a847eee9131b9a24d4102582fe6
Nathan Hwang <thenoviceoof@gmail.com>
2013-01-20 17:26:38 -0500

    Try and guess what routes/endpoints we'll need

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 from bottle import route, run
 
+@route('/add')
+def add_task():
+    return 'Add a task'
+
+@route('/list')
+def list_tasks():
+    return 'List some tasks'
+
 @route('/')
-def root():
-    return 'This is root'
+def get_task():
+    return 'Get a task to do'
 
 run(host='localhost', port=8080, debug=True)
