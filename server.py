9406abb3a96ed8738212e8bca2b7a0e6969a53b6
Nathan Hwang <thenoviceoof@gmail.com>
2013-01-23 00:43:18 -0500

    Thinking more about how to do the basic API

--- server.py
+++ server.py
@@ -1,15 +1,52 @@
 from bottle import route, run
 
-@route('/add')
+@get('/add')
 def add_task():
+    '''
+    Serve up the page to add a task
+    '''
+    # TODO: template
     return 'Add a task'
 
-@route('/list')
+@post('/add')
+def add_task_endpoint():
+    '''
+    Add a task
+    '''
+    task = request.forms.get('task')
+    tags = request.forms.get('tags')
+    tags = tags.split(',')
+    # create_task(task, tags, time.now(), order=None)
+    return ''
+
+@get('/list')
 def list_tasks():
+    '''
+    List tasks for ordering
+    '''
+    # TODO: template
     return 'List some tasks'
 
-@route('/')
+@post('/list')
+def order_tasks():
+    '''
+    Save the ordering of the tasks
+    '''
+    return ''
+
+@get('/')
 def get_task():
+    '''
+    Offer a couple tasks, along with fun, or choose from the backlog
+    '''
+    # TODO: template
     return 'Get a task to do'
+
+@post('/')
+def choose_task():
+    '''
+    POST back which task, and how long you will commit to it
+    '''
+    return ''
 
 run(host='localhost', port=8080, debug=True)
